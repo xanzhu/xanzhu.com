8e82246e28dabaeb33f790c65239d80e2d14b8b1
xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-01-27 20:39:40 +1300

    Minor tweaks - Refactor brand marquee (now a list) - Hero tweaks

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <main class="flex flex-col justify-center">
-        <HomeHero />
-        <LazyHomeCompany />
+        <V2HomeHero />
+        <LazyV2HomeBrandList />
         <LazyV2HomeBlogFeature class="min-h-screen" />
         <LazyHomePagesCard class="my-20" />
         <LazyV2HomeThreatCard class="mt-20" />
